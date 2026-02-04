cb9214e8e3004438b129e9338947595c57693813
Tony Lampada <tonylampada@gmail.com>
2026-02-04 16:33:04 -0300

    Step 4: Add divide function
    Part of #1

--- calculator.py
+++ calculator.py
@@ -4,3 +4,10 @@
 def add(a: float, b: float) -> float:
     """Add two numbers."""
     return a + b
+
+
+def divide(a: float, b: float) -> float:
+    """Divide a by b."""
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
